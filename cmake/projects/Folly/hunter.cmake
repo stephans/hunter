d133d1c2f3ccd03af113f15c601a0233cc01a524
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2018-10-30 13:19:24 -0700

    Cherry-picked GCC 8 warning fix for Folly.
    * Don't fail on warnings in Folly build.

--- cmake/projects/Folly/hunter.cmake
+++ cmake/projects/Folly/hunter.cmake
@@ -25,6 +25,20 @@ hunter_add_version(
   SHA1 e16dc7c5f62762cc7b88b7c9cf8bce3ee963fc18
 )
 
+hunter_add_version(
+  PACKAGE_NAME Folly
+  VERSION "2018.04.20.01"
+  URL "https://github.com/ambroff/folly/archive/9543e7d77b11af662c792be9390e2ad0b621040a.zip"
+  SHA1 1f524b68eb6edc158830ce6cbda7d07f16a8fcfa
+)
+
+hunter_add_version(
+  PACKAGE_NAME Folly
+  VERSION "2018.04.20.02"
+  URL "https://github.com/ambroff/folly/archive/748b0c4ecf0af5b03a575373b6cc7c2ab25e8794.zip"
+  SHA1 7e852c190c627691de3ba76b90fb6ac9f5343fbc
+)
+
 if (APPLE)
   hunter_cmake_args(Folly CMAKE_ARGS FOLLY_USE_LIBCPP=TRUE)
 endif()
