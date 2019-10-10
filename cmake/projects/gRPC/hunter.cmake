4a57d5b5ee4a757a1ae33e3ed0715fcf144916e3
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2019-10-10 16:56:16 -0700

    Cherry-pick that fix for 0.10.0 also.

--- cmake/projects/gRPC/hunter.cmake
+++ cmake/projects/gRPC/hunter.cmake
@@ -31,6 +31,12 @@ hunter_add_version(
 
 hunter_add_version(
     PACKAGE_NAME gRPC
+    VERSION "1.10.0-p1"
+    URL "https://github.com/ambroff/grpc/archive/hunter-1.10.0-p1.zip"
+    SHA1 "95d1273e0b204de781ef2519c306ccea28b90b01")
+
+hunter_add_version(
+    PACKAGE_NAME gRPC
     VERSION "1.12.1-p0"
     URL "https://github.com/hunter-packages/grpc/archive/v1.12.1-p0.tar.gz"
     SHA1 "cee253f1a9075afe7eb8061a2783f50368ff3495")
