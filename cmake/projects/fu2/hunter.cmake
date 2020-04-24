394f09dfdc0e8fe1392f9decfa0d4f050713ad5e
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2020-04-24 10:52:05 -0700

    Add version from my fu2 fork that allows disabling tests.

--- cmake/projects/fu2/hunter.cmake
+++ cmake/projects/fu2/hunter.cmake
@@ -19,7 +19,20 @@ hunter_add_version(
   e9d871aaebf96ec63eb7d2a5299cfc44f13d76fc
   )
 
-hunter_cmake_args(fu2 CMAKE_ARGS FU2_WITH_CPP_LATEST=ON)
+# List of versions here...
+hunter_add_version(
+  PACKAGE_NAME
+  fu2
+  VERSION
+  "4.1.0.1"
+  URL
+  "https://github.com/ambroff/function2/archive/4.1.0_hunter.tar.gz"
+  SHA1
+  e9d871aaebf96ec63eb7d2a5299cfc44f13d76fc
+  )
+
+
+hunter_cmake_args(fu2 CMAKE_ARGS FU2_WITH_CPP_LATEST=ON FU2_ENABLE_TESTS=OFF)
 
 # Pick a download scheme
 hunter_pick_scheme(DEFAULT url_sha1_cmake) # use scheme for cmake projects
