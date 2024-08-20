f73972fb2c60618564cd19d88d4ffd95d50b4a88
Stephan Soileau <stephan@tlde.io>
2024-08-20 13:58:28 -0700

    Add jemalloc 5.3.0

--- cmake/projects/jemalloc/hunter.cmake
+++ cmake/projects/jemalloc/hunter.cmake
@@ -8,7 +8,14 @@ hunter_add_version(
   VERSION "5.2.1"
   URL "https://github.com/jemalloc/jemalloc/releases/download/5.2.1/jemalloc-5.2.1.tar.bz2"
   SHA1 9e06b5cc57fd185379d007696da153893cf73e30
-  )
+)
+
+hunter_add_version(
+  PACKAGE_NAME jemalloc
+  VERSION "5.3.0"
+  URL "https://github.com/jemalloc/jemalloc/archive/refs/tags/5.3.0.tar.gz"
+  SHA1 3e4781e0c8c0033a0da5f93a204c172337c3353b
+)
 
 hunter_configuration_types(jemalloc CONFIGURATION_TYPES Release)
 hunter_pick_scheme(DEFAULT url_sha1_autotools)
